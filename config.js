50db9425de10922058d1f786724a313da1741a5d
Liseidy Bueno <liseidybueno@gmail.com>
2020-12-07 02:47:58 -0500

    change config file

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
-module.exports = {
+let config = {
   HOST: "us-cdbr-east-02.cleardb.com",
   USER: "b723f6aa651753",
   PASSWORD: "9f708db7",
   DB: "heroku_6538f3d60570385"
 };
+
+module.exports = config;
