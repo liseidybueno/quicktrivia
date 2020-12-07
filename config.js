27bdbeed29f3165502d2d78b28611116916718c2
Liseidy Bueno <liseidybueno@gmail.com>
2020-12-07 01:47:11 -0500

    mysql config to remote

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
-let config = {
-  host    : 'localhost',
-  user    : 'root',
-  password: 'AbC123XyZ321',
-  database: 'triviaquizdb'
-};
+// let config = {
+//   host    : 'localhost',
+//   user    : 'root',
+//   password: 'AbC123XyZ321',
+//   database: 'triviaquizdb'
+// };
+// module.exports = config;
 
-module.exports = config;
+module.exports = {
+  HOST: "us-cdbr-east-02.cleardb.com",
+  USER: "b723f6aa651753",
+  PASSWORD: "9f708db7",
+  DB: "heroku_6538f3d60570385"
+};
