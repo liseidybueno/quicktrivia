240d2f84f6cdb8e79ca77e538aead52961e0b9f2
Liseidy Bueno <liseidybueno@gmail.com>
2020-12-08 00:41:44 -0500

    add routes to url

--- routes/login.js
+++ routes/login.js
@@ -17,7 +17,7 @@ router.get("/login", function(req, res) {
   });
 });
 
-router.post("/login-get", function(req, res) {
+router.post("/login", function(req, res) {
 
   const username = req.body.username;
   const password = req.body.password;
