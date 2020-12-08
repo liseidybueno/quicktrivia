f1bbbbd1d4e6c2ff11910b87747e2950c5184711
Liseidy Bueno <liseidybueno@gmail.com>
2020-12-07 22:11:13 -0500

    Change DB

--- config.js
+++ config.js
@@ -7,10 +7,10 @@
 // module.exports = config;
 
 let config = {
-  host: "",
-  user: "",
-  password: "",
-  database: ""
+  host: "us-cdbr-east-02.cleardb.com",
+  user: "b02f444a30ab24",
+  password: "84e47103",
+  database: "heroku_33b59430916aed3"
 };
 
 module.exports = config;
