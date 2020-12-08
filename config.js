aaa8f25a8fc8b3e2837d929bd6671cb82d3bb2b7
Liseidy Bueno <liseidybueno@gmail.com>
2020-12-07 22:48:15 -0500

    create pool on DB

--- config.js
+++ config.js
@@ -7,18 +7,11 @@
 // module.exports = config;
 var mysql = require("mysql");
 
-let config = {
+var connection = mysql.createPool({
   host: "us-cdbr-east-02.cleardb.com",
   user: "b02f444a30ab24",
   password: "84e47103",
   database: "heroku_33b59430916aed3"
-};
-
-const connection = mysql.createPool(config);
-
-connection.connect(error => {
-  if(error) throw error;
-  console.log("Successfully connected to the database.");
 });
 
 module.exports = connection;
