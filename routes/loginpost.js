612d60e3d90728a08a627c3cfe0a50f540f41c6b
Liseidy Bueno <liseidybueno@gmail.com>
2020-12-09 23:36:18 -0500

    login post

--- routes/loginpost.js
+++ routes/loginpost.js
@@ -11,92 +11,94 @@ const saltRounds = 10;
 
 router.post("/loginpost", function(req, res) {
 
-  const username = req.body.username;
-  const password = req.body.password;
-  var fname = "";
-  var total_score = "";
+      const username = req.body.username;
+      const password = req.body.password;
+      var fname = "";
+      var total_score = "";
 
-  let user_stmt = "SELECT * FROM USERS WHERE username=?";
+      let user_stmt = "SELECT * FROM USERS WHERE username=?";
 
-  if(username == ""){
-    var login_error = "Username not found. Please enter a valid username or register.";
-    res.render("login", {curr_user: constants.curr_user, login_error: login_error});
-  } else {
-  //look for username
-  sql.query(user_stmt, username, (err, result, fields) => {
-    if(err){
-      console.log(err);
-    }else if (!err) {
-      console.log("Look for username");
-      //if there is no error, check if the username exists
-      //if it exists, then get the fname and make sure the password is correct
-      if (result.length > 0) {
-        fname = result[0].fname;
+      if (username == "") {
+        //username is blank
+        var login_error = "Username not found. Please enter a valid username or register.";
+        res.render("login", {
+          curr_user: constants.curr_user,
+          login_error: login_error
+        });
+      } else { //else if it is not blank
+        //look for username
+        sql.query(user_stmt, username, (err, result, fields) => {
+            if (err) {
+              console.log(err);
+            } else if (!err) {
+              //if the username exists, then see if passwords match
+              if (result.length > 0) {
+                fname = result[0].fname;
+                //get matching pw
 
-        bcrypt.compare(password, result[0].password, function(err, result) {
-          if (result === true) {
-            //if the password is correct, store the username and first name in the
-            //appropriate global variables
-            constants.curr_user.username = username;
-            constants.curr_user.fname = fname;
+                bcrypt.compare(password, result[0].password, function(err, result) {
+                    if (!err) {
+                      //if the result is true, then log person in
+                      if (result == true) {
+                        //if the password is correct, store the username and first name in the
+                        //appropriate global variables
+                        constants.curr_user.username = username;
+                        constants.curr_user.fname = fname;
 
-            //then, look through the score table for the total score
-            var score_stmt = "SELECT * FROM SCORES WHERE username = ?";
+                        var score_stmt = "SELECT * FROM SCORES WHERE USERNAME = ?";
+
+                        sql.query(score_stmt, username, (err, result, fields) => {
+                          if (!err) {
+                            //if the score exists, save it
+                            if (result.length > 0) {
+                              total_score = result[0].total_score;
+
+                              console.log("Password correct!");
+
+                              constants.curr_user.total_score = total_score;
+                              res.redirect("/");
+                              // res.send({
+                              //   redirect: true,
+                              //   redirect_url: "/"
+                              // });
+                            } else {
+                              total_score = 0;
+                              constants.curr_user.total_score = 0;
+                              res.redirect("/");
+                              // res.send({
+                              //   redirect: true,
+                              //   redirect_url: "/"
+                              // });
+                            }
+                          }
+
+                        });
+
+                      } else { //if result is not true, then send error message
+                        var login_error = "Your password is incorrect. Please try again.";
+                        res.render("login", {
+                          curr_user: constants.curr_user,
+                          login_error: login_error
+                        });
+
+                      }
+                    }
+                  });
 
 
-            sql.query(score_stmt, username, (err, result, fields) => {
-              if (!err) {
-                //if the score exists, save it
-                if (result.length > 0) {
-                  total_score = result[0].total_score;
-
-                  console.log("Password correct!");
-
-                  constants.curr_user.total_score = total_score;
-                  res.redirect("/");
-                  // res.send({
-                  //   redirect: true,
-                  //   redirect_url: "/"
-                  // });
-                } else {
-                  total_score = 0;
-                  constants.curr_user.total_score = 0;
-                  res.redirect("/");
-                  // res.send({
-                  //   redirect: true,
-                  //   redirect_url: "/"
-                  // });
+                }  else { //otherwise tell me the username doesn't exist
+                  var login_error = "This username does not exist";
+                  res.render("loginn", {
+                    curr_user: constants.curr_user,
+                    login_error: login_error
+                  });
                 }
+
               }
-
             });
+        }
+      });
 
-          } else {
-            constants.curr_user.username = "";
-            constants.curr_user.fname = "";
-            console.log("PW incorrect");
-            const login_error = "Sorry, your password is incorrect. Please try again!";
-            res.render("login", {login_error: login_error, curr_user: constants.curr_user,});
-            // res.send({
-            //   login_error: login_error
-            // });
-          }
-        }); //if it doesn't exist, then tell user their username is incorrect
-      } else {
-        constants.curr_user.username = "";
-        constants.curr_user.fname = "";
-        console.log("Username incorrect");
-        const login_error = "Sorry, that username does not exist. Please try again!";
-        res.render("login", {login_error: login_error, curr_user: constants.curr_user,});
-        // res.send({
-        //   login_error: login_error
-        // });
 
-      }
-    }
 
-  });
-}
-});
-
-module.exports = router;
+    module.exports = router;
