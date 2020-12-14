95ee2ffe5c76dad2c9ad815b9963f9dba0c95212
Liseidy Bueno <liseidybueno@gmail.com>
2020-12-13 22:19:41 -0500

    create environment variables

--- routes/emailpw.js
+++ routes/emailpw.js
@@ -9,17 +9,17 @@ const lib = require('./../index.js');
 const sql = require("./../config.js");
 const constants = require("./../constants.js");
 const router = express.Router();
-//
-// const oauth2Client = new OAuth2(
-//   "123745007922-tskba5l8jsmfv1ok8uteg2p9dj9f74lp.apps.googleusercontent.com", //clientID
-//   "KAstSetFa7GlZO2SlRFOrM1b", //client secret
-//   "https://developers.google.com/oauthplayground" //redirect URL
-// );
 
-// oauth2Client.setCredentials({
-//      refresh_token: "1//04X1kOdvgfp3kCgYIARAAGAQSNwF-L9Ir95SyguHfmcQhW4boc6bWogQDmO5tBtBsp0lsj59k-4H2-tmJrJKvxVllrZwmFBMIBXs"
-// });
-// const accessToken = oauth2Client.getAccessToken()
+const oauth2Client = new OAuth2(
+  process.env.CLIENTID, //clientID
+  process.env.CLIENTSECRET, //client secret
+  "https://developers.google.com/oauthplayground" //redirect URL
+);
+
+oauth2Client.setCredentials({
+     refresh_token: process.env.REFRESHTOKEN
+});
+const accessToken = oauth2Client.getAccessToken()
 
 router.post("/emailpw", function(req, res) {
 
@@ -55,17 +55,17 @@ router.post("/emailpw", function(req, res) {
           service: "gmail",
           auth: {
             type: "OAuth2",
-            user: "liseidybueno@gmail.com",
-            clientId: "469086209980-bh0m4qnc1slafpv5iuktf1j4j7bq3b1t.apps.googleusercontent.com",
-            clientSecret: "AXc5hXYAKS-fEx9IdKGjgh4B",
-            refreshToken: "1//04JuMpHhAtBC5CgYIARAAGAQSNwF-L9Ir-Pw7i00N0C3eZjy9sZum2fxhlKJQeDQ62NxpU_4Shi5dE6TZeHgsd8KerZt64gGLiEE",
-            accessToken: "ya29.a0AfH6SMB8ofqYLwCX5fUBmrFsRhwZSFveGqXuFcyQLwvZ4JToLn8YDTP3YL8m_E41lYLag5_R0KEtaooa8cOhxgFnHHf9JVMS1GuAywCNRV-0Lb-ITo1HXvOFTr4FIDcIXiyCfq1EGq82JcV4uGPWVGJrUi6F3O9QBqL6ubHPK_c"
+            user: process.env.EMAIL,
+            clientId: process.env.CLIENTID,
+            clientSecret: process.env.CLIENTSECRET,
+            refreshToken: process.env.REFRESHTOKEN,
+            accessToken: accessToken
           }
         });
 
 
         var mailOptions = {
-          from: 'liseidybueno@gmail.com',
+          from: process.env.EMAIL,
           to: email,
           subject: 'Quick Trivia Reset Password',
           html: '<p>Hi, ' + fname + '!</p><p>Your username is: ' + username + '</p><p>Click here to reset password: <a href="hhttps://quicktrivia.herokuapp.com/createnew">Create New</a>'
@@ -75,7 +75,6 @@ router.post("/emailpw", function(req, res) {
           if (error) {
             console.log(error);
           } else {
-            console.log("email sent");
             var error_msg = new Array();
             error_msg.push("Your email has been sent!");
             res.render("resetpassword", {
@@ -96,22 +95,6 @@ router.post("/emailpw", function(req, res) {
     });
   }
   }
-
-
-
-// var transporter = nodemailer.createTransport({
-//   host: 'smtp.gmail.com',
-//   port: 465,
-//   secure: true,
-//   auth: {
-//     type: 'OAuth2',
-//         user: process.env.EMAIL,
-//         clientId: "123745007922-tskba5l8jsmfv1ok8uteg2p9dj9f74lp.apps.googleusercontent.com",
-//         clientSecret: "KAstSetFa7GlZO2SlRFOrM1b",
-//         refreshToken: "1//04X1kOdvgfp3kCgYIARAAGAQSNwF-L9Ir95SyguHfmcQhW4boc6bWogQDmO5tBtBsp0lsj59k-4H2-tmJrJKvxVllrZwmFBMIBXs",
-//        accessToken: accessToken
-//   }
-// });
 
   });
 
